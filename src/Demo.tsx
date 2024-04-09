b363c88ce53a8b0510246ea7f83af7288cc5eadd
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-09 14:23:12 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import Button from '@mui/joy/Button';
-import Input from '@mui/joy/Input';
+
 
 
 export default function ButtonUsage() {
   return <Button variant="solid">Hello world</Button>;
 }
 
-export default function BasicInput() {
-  return <Input placeholder="Type in here…" />;
-}
+
