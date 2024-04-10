820cb750d13a2380553344343176859ae31e3327
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-10 14:38:53 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import Card from '@mui/joy/Card';
+import Typography from '@mui/joy/Typography';
+
 
 
 
 export default function SignInSheet() {
-  return ( <Card />
+  return ( 
+    <Card sx ={{ width: 300, height: 300 }}>
+      <div>
+        <Typography level="title-lg">Welcome to BoardX!</Typography>
+      </div>
+    
+  
+  </Card>
           );
           }
     
