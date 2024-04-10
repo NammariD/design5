a4d677218651021119f227cd9c1936b4b79fe500
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-10 18:43:11 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -3,6 +3,8 @@ import Card from '@mui/joy/Card';
 import Typography from '@mui/joy/Typography';
 import Email from './components/Email';
 import Password from './components/Password';
+import AspectRatio from '@mui/joy/AspectRatio';
+
 
 
 
