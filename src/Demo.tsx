e35c29c884af86db1cd1e4b281ad79a65649bf74
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-10 14:17:19 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,58 +1,46 @@
 import * as React from 'react';
 import Box from '@mui/joy/Box';
-import List from '@mui/joy/List';
-import ListItem from '@mui/joy/ListItem';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import ListItemButton from '@mui/joy/ListItemButton';
+import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
+import Sheet from '@mui/joy/Sheet';
+import Chip from '@mui/joy/Chip';
 import Typography from '@mui/joy/Typography';
 
+const theme = extendTheme({ cssVarPrefix: 'demo' });
 
-
-export default function SizesList() {
-   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        display: 'flex',
-        justifyContent: 'center',
-        gap: 2,
-        flexWrap: 'wrap',
-        '& > *': { minWidth: 0, flexBasis: 200 },
-      }}
+export default function DarkModeByDefault() {
+  return (
+    <CssVarsProvider
+      defaultMode="dark"
+      // the props below are specific to this demo,
+      // you might not need them in your app.
+      //
+      theme={theme}
+      // the selector to apply CSS theme variables stylesheet.
+      colorSchemeSelector="#demo_dark-mode-by-default"
+      //
+      // the local storage key to use
+      modeStorageKey="demo_dark-mode-by-default"
+      //
+      // set as root provider
+      disableNestedContext
     >
-      {(['sm', 'md', 'lg'] as const).map((size) => (
-        <div key={size}>
-          <Typography level="body-xs" mb={2}>
-            <code>size=&quot;{size}&quot;</code>
-          </Typography>
-          <List
-            size={size}
-            variant="outlined"
-            sx={{
-              maxWidth: 300,
-              borderRadius: 'sm',
-            }}
+      <div id="demo_dark-mode-by-default">
+        <Sheet sx={{ px: 3, py: 1.5, borderRadius: 'sm' }}>
+          <Typography
+            component="div"
+            endDecorator={
+              <Chip variant="outlined" color="primary" size="sm">
+                Default
+              </Chip>
+            }
+            fontSize="lg"
           >
-            <ListItem>
-              <ListItemButton>
-                <ListItemDecorator>
-                  
-                </ListItemDecorator>
-                Home
-              </ListItemButton>
-            </ListItem>
-            <ListItem>
-              <ListItemButton>Projects</ListItemButton>
-            </ListItem>
-            <ListItem>
-              <ListItemButton>Settings</ListItemButton>
-            </ListItem>
-          </List>
-        </div>
-      ))}
-    </Box>
+            Dark mode
+          </Typography>
+        </Sheet>
+      </div>
+    </CssVarsProvider>
   );
-  
 }
 
 
