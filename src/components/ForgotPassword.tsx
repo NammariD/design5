9e8617b942c5fbde78d0933e41ec18c187baf926
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-11 17:14:49 -0400

    Update ForgotPassword.tsx

--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -3,6 +3,7 @@ import Button from '@mui/joy/Button';
 
 export default function BasicButtons() {
   return (
-    <Button sx = {{ width: 550, color: '#D8819E'}} variant = "plain" >Forgot Password</Button>
+    <Button sx = {{ width: 550, color: '#D8819E','&:hover': {
+          backgroundColor: '#430A1F',},}} variant = "plain" >Forgot Password</Button>
     );
 }
