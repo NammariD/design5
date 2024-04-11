637b8358f63504bb23c6475c3b596b189c0ff921
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-11 16:11:26 -0400

    Update Password.tsx

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import Input from '@mui/joy/Input';
 
 export default function InfoBar() {
-  return <Input placeholder="Password" />;
+  return <Input placeholder="Password" sx = {{ backgroundColor: '0B0D0E'}} />;
 }
 
