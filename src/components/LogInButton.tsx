fb1ef8be108a7130e832919c08274630c2f87029
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-11 17:11:25 -0400

    Update LogInButton.tsx

--- src/components/LogInButton.tsx
+++ src/components/LogInButton.tsx
@@ -3,6 +3,7 @@ import Button from '@mui/joy/Button';
 
 export default function BasicButtons() {
   return (
-    <Button sx = {{ width: 550, backgroundColor: '#F21D6B'}}  >Log In</Button>
+    <Button sx = {{ width: 550, backgroundColor: '#F21D6B', '&:hover': {
+          backgroundColor: '#FF4081',}, }}  >Log In</Button>
     );
 }
