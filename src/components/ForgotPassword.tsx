0f8890eaf646e4c1894cfa004284d400361f4cdf
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-11 14:02:25 -0400

    Update ForgotPassword.tsx

--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -1,1 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/joy/Button';
 
+export default function BasicButtons() {
+  return (
+    <Button sx = {{ width: 550}} >Forgot Password</Button>
+    );
+}
