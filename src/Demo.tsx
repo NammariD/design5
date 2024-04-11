ed5fa139735f3e61c50b32e2d02fe95925cef113
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-11 18:04:16 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -12,7 +12,7 @@ export default function SignInSheet() {
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', padding: '15px', backgroundColor: '#121212' }}>
       <Card sx={{ width: 550, height: 406, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '20px', backgroundColor: '#121212'}}>
         <div style={{ width: '100%', textAlign: 'center' }}>
-          <img src= './src/images/boardx logo.png' alt="BoardX Logo" />
+          <img src= 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DQI_4f4s3JNA&psig=AOvVaw2YMa4lAnrloA7YV74qXWre&ust=1712959422457000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPjLnqGVu4UDFQAAAAAdAAAAABAE' alt="BoardX Logo" />
 
           <Typography>Welcome to BoardX!</Typography>
           <div style={{ margin: '20px 0' }}>
