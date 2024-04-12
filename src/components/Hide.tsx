3fb25ebe0aad87facd3dc78ebbe4d391204ab896
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-11 20:51:44 -0400

    Update Hide.tsx

--- src/components/Hide.tsx
+++ src/components/Hide.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import Button from '@mui/joy/Button';
 import SvgIcon from '@mui/joy/SvgIcon';
+import IconButton from '@mui/joy/IconButton';
+
 
 export default function Hide() {
   return(
-    <Button startDecorator = { 
-      <SvgIcon>
-        <svg  xmlns="http://www.w3.org/2000/svg">
-          
-        </svg>
-      </SvgIcon>
-    }
-    </Button>
+    <IconButton
+  sx={{
+    "--IconButton-size": "35px"
+  }}
+>
+  <FavoriteBorder />
+</IconButton>
     );
 }
 
