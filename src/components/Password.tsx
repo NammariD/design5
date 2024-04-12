328560c5c6848272be90bdd133d58047d3a41883
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-12 12:38:39 -0400

    Update Password.tsx

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'; 
 import Input from '@mui/joy/Input';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import Button from '@mui/joy/Button';
 
 
 export default function InfoBar() {
   return (
-    <Input type = "password" sx={{ backgroundColor: '#0B0D0E' }} placeholder="Password"  />
+    <Input type = "password" sx={{ backgroundColor: '#0B0D0E' }} placeholder="Password" endDecorator = { <Button startDecorator = {<VisibilityIcon/>}/>
   );
 }
