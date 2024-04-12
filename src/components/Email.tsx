fe411198048f1d5e48858a12b4d09bc673cbdbf8
NammariD <157739804+NammariD@users.noreply.github.com>
2024-04-11 20:31:19 -0400

    Update Email.tsx

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Input from '@mui/joy/Input';
 
 export default function InfoBar() {
-  return <Input sx = {{ backgroundColor: '#0B0D0E'}} placeholder="Email" />;
+  return <Input sx = {{ backgroundColor: '#0B0D0E',
+    '--Input-focusedHighlight': 'gray'}} placeholder="Email" />;
 }
 
